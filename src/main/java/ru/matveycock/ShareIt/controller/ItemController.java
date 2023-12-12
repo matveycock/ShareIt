9cc81e910a974adf83e474edfe169ff052653d71
matveycock <chaccki@gmail.com>
2023-12-12 14:44:20 +0300

    add: add skeletons of controller`s methods

--- src/main/java/ru/matveycock/ShareIt/controller/ItemController.java
+++ src/main/java/ru/matveycock/ShareIt/controller/ItemController.java
@@ -1,7 +1,37 @@
 package ru.matveycock.ShareIt.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.matveycock.ShareIt.model.Item;
+
+import java.util.List;
 
 @RestController("/item")
 public class ItemController {
+
+
+    @GetMapping
+    public List<Item> getAllItems(){
+        return null;
+    }
+
+
+    @GetMapping("/{id}")
+    public Item getItembyID( long id){
+        return null;
+    }
+
+    @PostMapping
+    public void addNewItem(){
+
+    }
+
+    @PostMapping("/{id}")
+    public void updateItemByID(){
+
+    }
+
+
 }
--- src/main/java/ru/matveycock/ShareIt/controller/UserController.java
+++ src/main/java/ru/matveycock/ShareIt/controller/UserController.java
@@ -1,7 +1,34 @@
 package ru.matveycock.ShareIt.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.matveycock.ShareIt.model.User;
 
-@RestController("users")
+import java.util.List;
+
+@RestController("/users")
 public class UserController {
+
+    @GetMapping("/{id}")
+    public User getUserById(long id){
+        return null;
+    }
+
+    @GetMapping()
+    public List<User> getAllUsers(){
+        return null;
+    }
+
+    @PostMapping("")
+    public void addNewUser(){
+
+    }
+
+    @PatchMapping("/{id")
+    public void updateUser(long id){
+
+    }
+
 }
