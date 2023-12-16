c7c2d9b5146e497e262b874f78a27dff89a93b27
matveycock <chaccki@gmail.com>
2023-12-16 15:48:33 +0300

    add: add logic of user

--- src/main/java/ru/matveycock/ShareIt/controller/UserController.java
+++ src/main/java/ru/matveycock/ShareIt/controller/UserController.java
@@ -1,32 +1,44 @@
 package ru.matveycock.ShareIt.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.matveycock.ShareIt.model.User;
+import ru.matveycock.ShareIt.service.UserService;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
 
+    private UserService service;
+
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable long id){
-        return null;
+    public User findUserByID(@PathVariable long id){
+        return service.findUserByID(id);
     }
 
     @GetMapping
-    public List<User> getAllUsers(){
-        return null;
+    public List<User> findAllUsers(){
+        return service.findAllUsers();
     }
 
     @PostMapping
-    public void addNewUser(){
-
+    public User saveNewUser(User user) {
+        return service.saveNewUser(user);
     }
 
-    @PostMapping("/{id}")
-    public void updateUserByID(@PathVariable long id){
+    @PutMapping("/{id}/update")
+    public User updateUserByEmail(@RequestBody User user){
+        return service.updateUserByEmail(user);
+    }
 
+    @DeleteMapping("/{id}/delete")
+    public void deleteUserByID(@PathVariable long id){
+        service.deleteUserByID(id);
     }
 
 
--- src/main/java/ru/matveycock/ShareIt/service/UserService.java
+++ src/main/java/ru/matveycock/ShareIt/service/UserService.java
@@ -1,8 +1,21 @@
 package ru.matveycock.ShareIt.service;
 
 import org.springframework.stereotype.Service;
+import ru.matveycock.ShareIt.model.User;
 
-@Service
-public class UserService {
+import java.util.EnumMap;
+import java.util.List;
 
+
+public interface UserService {
+
+    User findUserByID(long id);
+
+    List<User> findAllUsers();
+
+    User saveNewUser(User user);
+
+    User updateUserByEmail(User user);
+
+    void deleteUserByID(long id);
 }
